bf6258e45bebb9165c039c68adaeeb4217fedd36
sayemaa <sayema990@gmail.com>
2022-04-27 20:53:17 +0600

    Initial commit

---
+++ __steps.js
@@ -0,0 +1,21 @@
+/* 
+
+    1. Create a folder (manually or mkdir)
+    2. Open command line to that folder
+    3. npm init -y
+    4. npm install express
+    5. Create index.js file
+
+    ---------------------
+    For Express/ Node Server
+    ---------------------
+
+    1. Require express
+    2. Create app variable 
+    3. Declare port
+    4. Set app.get('/')
+    5. Listen to port
+    6. node index.js
+    7. Check your browser for that port
+
+*/
---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+var cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Node')
+});
+
+const users = [
+    { id: 1, name: 'Ninguang', email: 'eulalawrence@gmail.com', phone: '0178888888' },
+    { id: 2, name: 'Qiqi', email: 'jean@gmail.com', phone: '0178888888' },
+    { id: 3, name: 'Mona', email: 'amber@gmail.com', phone: '0178888888' },
+    { id: 4, name: 'Diona', email: 'kaeya@gmail.com', phone: '0178888888' },
+    { id: 5, name: 'Xingqui', email: 'lisa@gmail.com', phone: '0178888888' },
+    { id: 6, name: 'Xiangling', email: 'bennett@gmail.com', phone: '0178888888' },
+    { id: 7, name: 'Keqing', email: 'fischl@gmail.com', phone: '0178888888' },
+]
+
+app.get('/users', (req, res) => {
+    /* filter by search query parameter */
+    if (req.query.name) {
+        const search = req.query.name.toLowerCase();
+        const matched = users.filter(user => user.name.toLowerCase().includes(search))
+        res.send(matched)
+    }
+    else {
+        res.send(users);
+
+    }
+})
+
+app.get('/user/:id', (req, res) => {
+    console.log(req.params);
+    const id = parseInt(req.params.id);
+    const user = users.find(u => u.id === id);
+    res.send(user);
+})
+
+app.post('/user', (req, res) => {
+    console.log('request', req.body)
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user);
+    res.send(user);
+})
+
+app.get('/fruits', (req, res) => {
+    res.send(['mango', 'apple', 'oranges']);
+})
+
+app.get('/fruits/mango/fazle', (req, res) => {
+    res.send('mango fazle flavor');
+})
+
+app.listen(port, () => {
+    console.log('Listening to port', port);
+})
